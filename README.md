bb7f6cd652d4b3a4feecbb313ff4c589abd68fdd
Ronald Xavier <110365605+RonaldLHR@users.noreply.github.com>
2024-12-04 20:16:30 +0000

    Adiciona Scitpts, lint:check e Lint:fix

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
-Implementação do  https://www.tabnews.com.br/ para o  https://curso.dev, aprendendo a fazer um projeto do zero com Felipedeschamps
+
+Implementação do https://www.tabnews.com.br/ para o https://curso.dev, aprendendo a fazer um projeto do zero com Felipedeschamps
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
-function Home () {
-    return <h1>Ana, eu amo você, se você me ama, da uma risadinha!❤️</h1>
+function Home() {
+  return <h1>Ana, eu amo você, se você me ama, da uma risadinha!❤️</h1>;
 }
 
-export default Home;  
+function teste() {
+  console.log("teste");
+}
+function teste() {
+  console.log("indentação errada");
+}
+
+export default Home;
