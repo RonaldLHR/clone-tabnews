7c81025f48b0a81d1ec61cedbf0b1862aa80b899
Ronald Xavier <ronaldx563@gmail.com>
2024-12-05 12:39:06 +0000

    Adicionando commit de pages

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,7 @@
-function Home() {
-  return <h1>Ana, eu amo você, se você me ama, da uma risadinha!❤️</h1>;
+function Home1() {
+  return (
+    <h1>Ana eu amo você meu amor, você me ama, se sim da uma risadinha!</h1>
+  );
 }
 
-function teste() {
-  console.log("teste");
-}
-function teste() {
-  console.log("indentação errada");
-}
-
-export default Home;
+export default Home1;
